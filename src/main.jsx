5bd13afb626a31573de21f1f64673ba490fd0427
AbdulazizRus <coderdim@gmail.com>
2024-07-05 20:52:00 +0500

    path config finishedd

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Root from './root'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <h1>crm</h1>
-  </React.StrictMode>,
+    <Root />
+  </React.StrictMode>
 )
